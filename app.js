fbe7884ea04aa66b2caa3bc308054df8325b31d0
sophiamenez <sophia.menezes@sptech.school>
2026-05-25 12:23:47 -0300

    Subindo atualizações no css

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
-
 app.use("/quiz", quizRouter);
 
 app.listen(PORTA_APP, function () {
