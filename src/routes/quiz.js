763057728b58ae0d1dcf96a64b0007df8138108c
sophiamenez <sophia.menezes@sptech.school>
2026-05-28 15:24:41 -0300

    Subindo atulizações no código

--- src/routes/quiz.js
+++ src/routes/quiz.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 var quizController = require("../controllers/quizController");
 
-router.post("/salvar", function (req, res){
-    quizController.guardarResposta(req, res);
-});
-
 router.get("/resultado/:fkUsuario", function(req, res){
     quizController.mostrarResultado(req, res);
+});
+
+router.post("/salvar", function (req, res){
+    quizController.guardarResposta(req, res);
 });
 
 module.exports = router;
